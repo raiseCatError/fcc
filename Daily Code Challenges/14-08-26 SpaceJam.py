7d2b5b0929af85efdb2c78ff378099dbb26969a4
raiseCatError <315733358+raiseCatError@users.noreply.github.com>
2026-08-14 19:05:23 +0530

    Add fcc Daily Code Challenges

---
+++ Daily Code Challenges/14-08-26 SpaceJam.py
@@ -0,0 +1,21 @@
+# S P A C E J A M
+# Given a string, remove all spaces from the string, insert two spaces between every character, convert all alphabetical letters to uppercase, and return the result.
+# Non-alphabetical characters should remain unchanged (except for spaces).
+
+
+def space_jam(s):
+    sol = []
+    for x in s:
+        if x != ' ':
+            sol.append(x.upper())
+            
+    sol = "  ".join(sol)
+
+    return sol
+
+
+space_jam("   free   Code   Camp   ")
+
+
+        
+
