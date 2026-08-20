c9275919df00aaaadcb00e65e91c3c1a5fe88fe9
raiseCatError <315733358+raiseCatError@users.noreply.github.com>
2026-08-20 22:17:00 +0530

    Refactor anagram checker

--- Daily Code Challenges/16-08-26 Anagram Checker.py
+++ Daily Code Challenges/16-08-26 Anagram Checker.py
@@ -7,15 +7,15 @@
 
 def are_anagrams(str1, str2):
 
-    str1 = str1.replace(" ", "")
-    str2 = str2.replace(" ", "")
+    str1 = str1.replace(" ", "").lower()
+    str2 = str2.replace(" ", "").lower()
 
     l1 = len(str1)
 
     check_count = 0
 
-    for x in str1.lower():
-        if x in str2.lower():
+    for x in str1:
+        if str1.count(x) == str2.count(x):
             check_count += 1
 
     return check_count == l1
